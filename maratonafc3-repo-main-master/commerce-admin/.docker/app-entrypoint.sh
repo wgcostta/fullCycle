118de3c5ff598ef6c7458d63c93f54ccce05f842
Wagner <wg.o.costa@gmail.com>
2020-07-03 23:53:50 -0300

    Projeto FullCycle Maratona 3.0

---
+++ maratonafc3-repo-main-master/commerce-admin/.docker/app-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -f ".env" ]; then
+  cp .env.example .env
+fi
+
+pip install --cache-dir=/home/django/app/.docker/.pip -r requirements.txt
+python manage.py migrate
+python manage.py loaddata initial_data
+python manage.py loaddata fake_data
+python manage.py sync_tenant_permissions
+python manage.py runserver 0.0.0.0:8000
